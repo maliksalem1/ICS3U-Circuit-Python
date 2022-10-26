2f9e21668a5271f3269b9338517cc82fb02ceb3b
Malik <99354681+maliksalem1@users.noreply.github.com>
2022-10-26 10:37:48 -0400

    code

---
+++ code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: maliksalem1
+# Created on: Oct 2022
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+
+def game_scene():
+    # This function is the main game game_scene
+
+    print("\n\n\n")
+    print("Hello, world!")
+
+    print("\nDone.")
+
+    # repeat forever, game loop
+    while True:
+        pass  # placeholder
+
+
+if __name__ == "__main__":
+    game_scene()
