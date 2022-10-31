77e6dca45c18eb778b3112ea555fa078ca3bc04f
Malik <99354681+maliksalem1@users.noreply.github.com>
2022-10-31 10:38:25 -0400

    code

--- code.py
+++ code.py
@@ -4,17 +4,19 @@
 # Created on: Oct 2022
 # This program is the "Space Aliens" program on the PyBadge
 
-import ugame
 import stage
+import ugame
 
 
 def game_scene():
     # This function is the main game game_scene
 
-    print("\n\n\n")
-    print("Hello, world!")
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    background = stage.Grid(image_bank_background, 10, 8)
 
-    print("\nDone.")
+    game = stage.Stage(ugame.display, 60)
+    game.layers = [background]
+    game.render_block()
 
     # repeat forever, game loop
     while True:
