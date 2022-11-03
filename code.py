7d39ee8efa7e4f6ccad0993d14d60549f93f30c5
Malik <99354681+maliksalem1@users.noreply.github.com>
2022-11-03 10:39:18 -0400

    code

--- code.py
+++ code.py
@@ -12,16 +12,41 @@ def game_scene():
     # This function is the main game game_scene
 
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
     background = stage.Grid(image_bank_background, 10, 8)
 
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
     game.render_block()
 
     # repeat forever, game loop
     while True:
-        pass  # placeholder
+        # get user input
+        keys = ugame.buttons.get_pressed()
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
+
+        game.render_sprites([ship])
+        game.tick()
 
 
 if __name__ == "__main__":
     game_scene()
+    
